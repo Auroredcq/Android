630c1dc70cbde393a7dd81c9ad3a867eafc465c7
aurore <auroredebacq83@gmail.com>
2022-03-04 16:28:57 +0100

    04/03

--- app/src/main/java/fr/isen/DEBACQ/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/DEBACQ/androiderestaurant/HomeActivity.kt
@@ -1,14 +1,37 @@
-package fr.isen.DEBACQ.androiderestaurant
+package fr.isen.debacq.androiderestaurant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.isen.debacq.androiderestaurant.databinding.ActivityHomeBinding
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : BaseActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.starter.setOnClickListener {
+            startCategoryActivity(ItemType.STARTER)
+        }
+
+        binding.main.setOnClickListener {
+            startCategoryActivity(ItemType.MAIN)
+        }
+
+        binding.dessert.setOnClickListener {
+            startCategoryActivity(ItemType.DESSERT)
+        }
+    }
+
+    private fun startCategoryActivity(item: ItemType) {
+        val intent = Intent(this, CategoryActivity::class.java)
+        intent.putExtra(CATEGORY_NAME, item)
+        startActivity(intent)
+    }
+
+    companion object {
+        const val CATEGORY_NAME = "CATEGORY_NAME"
     }
 }
-
-
-
